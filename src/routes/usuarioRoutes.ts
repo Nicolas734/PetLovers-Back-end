fb567ddcce38790e72d34a47c5c6dcdcc257ac09
Nicolas Lima <nichollaslimma734@gmail.com>
2023-05-19 22:15:13 -0300

    alterado roda de busca do usuario

--- src/routes/usuarioRoutes.ts
+++ src/routes/usuarioRoutes.ts
@@ -5,7 +5,7 @@ import { authenticate } from "../middlewares";
 const routes = Router();
 
 routes.get('/buscar', UsuarioController.buscarUsuarios);
-routes.get('/buscar/:id', authenticate , UsuarioController.buscarUsuario);
+routes.get('/buscar-info', authenticate , UsuarioController.buscarUsuario);
 routes.post('/cadastrar', UsuarioController.cadastrarUsuario);
 routes.put('/atualizar/:id', authenticate , UsuarioController.atualizarInfosDoUsuario);
 routes.delete('/excluir/:id', authenticate , UsuarioController.excluirUsuario)
