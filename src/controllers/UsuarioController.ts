4065ca900a076b10374f5503ce0912a518fa8155
Nicolas Lima <nichollaslimma734@gmail.com>
2023-05-19 22:10:38 -0300

    feito alteração na busca do usuario

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -24,7 +24,7 @@ class UsuarioController{
 
     public async buscarUsuario(req: Request, res: Response){
         try{
-            const id = req.params.id;
+            const id = res.locals.jwtPayload._id
             if(!idEhValido(id)){
                 return res.status(400).json({message: `id ${id} não é valido...`});
             }
